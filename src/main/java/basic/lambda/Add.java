65284dabec34b276de5c338599eb2046d5b66326
aycish <aycish07@gmail.com>
2022-02-10 19:59:57 +0900

    자바 람다표현식 기본 내용 정리

---
+++ src/main/java/basic/lambda/Add.java
@@ -0,0 +1,5 @@
+package basic.lambda;
+
+public interface Add {
+	int add(int x, int y);
+}
---
+++ src/main/java/basic/lambda/ExampleTest.java
@@ -0,0 +1,14 @@
+package basic.lambda;
+
+public class ExampleTest {
+
+	public static void main(String[] args) {
+		/* 인터페이스에 선언해놓은 메서드 람다식으로 구현 */
+		Add add = (x,y)-> { return x + y; };
+
+		/* return문 포함하여 생략 가능 */
+		//Add add = (x,y)-> x + y;
+
+		System.out.println(add.add(3 , 5));
+	}
+}
